9fe0173d78f8b59ea71eff45678b35bc84a08dbe
ilyasrasyid12 <ilyassr912@gmail.com>
2025-01-27 22:51:24 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerIcon = document.getElementById('hamburger-icon');
+  const slidingMenu = document.getElementById('sliding-menu');
+  const overlay = document.getElementById('overlay');
+  const closeBtn = document.getElementById('close-btn');
+
+  // Fungsi untuk menampilkan dan menyembunyikan sliding menu
+  hamburgerIcon.addEventListener('click', () => {
+      console.log("Hamburger icon clicked!");
+      slidingMenu.classList.toggle('translate-x-0');  // Tampilkan menu
+      slidingMenu.classList.toggle('translate-x-full');  // Sembunyikan menu
+      overlay.classList.toggle('hidden');  // Tampilkan overlay
+  });
+
+  closeBtn.addEventListener('click', () => {
+      slidingMenu.classList.add('translate-x-full');  // Sembunyikan menu
+      slidingMenu.classList.remove('translate-x-0');  // Pastikan kelas 'translate-x-0' hilang
+      overlay.classList.add('hidden');  // Sembunyikan overlay
+  });
+
+  overlay.addEventListener('click', () => {
+      slidingMenu.classList.add('translate-x-full');  // Sembunyikan menu
+      slidingMenu.classList.remove('translate-x-0');  // Pastikan kelas 'translate-x-0' hilang
+      overlay.classList.add('hidden');  // Sembunyikan overlay
+  });
+
+  // Swiper initialization
+  var swiper = new Swiper('.mySwiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 2,
+      },
+    },
+  });
+});
